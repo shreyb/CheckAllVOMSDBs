1b04f12e1d51161d954ab166afec1b08e5c0b533
sbhat <sbhat@fnal.gov>
2016-12-09 12:56:24 -0600

    Fixed user flag portion

--- check_all_voms.sh
+++ check_all_voms.sh
@@ -9,10 +9,9 @@ fi
 while [[ $# -gt 0 ]]
 do
 	key=$1
-	case key in 
+	case $key in 
 		-u)
 			USERNAME=$2
-			echo $USERNAME
 			shift 2
 			;;
 		-r)
@@ -27,18 +26,20 @@ do
 	esac	
 done
 
+# Figure out the search string
+if [[ -n $USERNAME ]] ; then
+	SEARCHSTRING="%CN=UID:$USERNAME"
+else
+	SEARCHSTRING="%$SEARCHSTRING%"
+fi
+
+# Get list of VOMS DBs
 VOMSDBS=`mysql  --defaults-extra-file=/usr/local/bin/gco_random/voms/.secret -e 'show databases;' | grep -G "voms"`
 
+# For each DB, get groups/roles, certs
 for DB in ${VOMSDBS};
 do 
 	echo "VO DATABASE: $DB"
-
-	if [[ -n $USERNAME ]] ; then
-		SEARCHSTRING="%CN=UID:$USERNAME"
-	else
-		SEARCHSTRING="%$SEARCHSTRING%"
-	fi
-
 	groups_roles=`mysql --defaults-extra-file=/usr/local/bin/gco_random/voms/.secret -s -N -D $DB -e "SELECT CONCAT(g.dn, ' ', r.role, '@@')  FROM usr AS u JOIN m JOIN groups AS g JOIN roles AS r WHERE u.userid = m.userid AND g.gid = m.gid AND r.rid = m.rid AND u.dn like '$SEARCHSTRING'"`
 	certs=`mysql --defaults-extra-file=/usr/local/bin/gco_random/voms/.secret -s -N -D $DB -e "SELECT CONCAT(c.subject_string,'@@') FROM certificate c INNER JOIN usr u on u.userid = c.usr_id WHERE u.dn like '$SEARCHSTRING'"`
 
@@ -59,7 +60,6 @@ do
 	fi
 	echo -e "\n"
 done
-
 
 
 # Ideas for dev:
